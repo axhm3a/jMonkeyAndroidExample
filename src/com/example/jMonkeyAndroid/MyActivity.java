c9c9ebb71ce80fa102fd7d559b96c51d3d7e3f4b
Daniel Basten <axhm3a@gmail.com>
2014-02-18 10:26:16 +0100

    removed unnecessary lines

--- src/com/example/jMonkeyAndroid/MyActivity.java
+++ src/com/example/jMonkeyAndroid/MyActivity.java
@@ -7,20 +7,13 @@ import com.jme3.system.android.AndroidConfigChooser;
 
 public class MyActivity extends AndroidHarness {
     public MyActivity() {
-
         appClass = "org.axhm3a.jmonkey.DesktopExample";
-
         eglConfigType = AndroidConfigChooser.ConfigType.BEST.BEST;
-// Exit Dialog title & message
         exitDialogTitle = "Exit?";
         exitDialogMessage = "Press Yes";
-// Edit: 25.06.2011: Enable verbose logging
         eglConfigVerboseLogging= true;
-// Edit: 30.06.2011: Choose screen orientation
         screenOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE;
-// Edit 12.07.2011: Invert the MouseEvents X (default = true)
         mouseEventsInvertX = true;
-// Edit 05.07.2011: Invert the MouseEvents Y (default = true)
         mouseEventsInvertY = true;
     }
 
@@ -30,7 +23,5 @@ public class MyActivity extends AndroidHarness {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.main);
-        //this.app.start();
     }
 }
